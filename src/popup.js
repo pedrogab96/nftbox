a694a1f37180a150e715d0ed22f38ee6b0ebf30a
Pedro Gabriel <pedrogab96@gmail.com>
2021-08-30 03:29:57 -0300

    added convert coins

--- src/popup.js
+++ src/popup.js
@@ -2,6 +2,28 @@ import $ from 'jquery';
 import groups from './groups';
 import moment from 'moment';
 
+let le = 70;
+let pvu = 1;
+let brl = 0;
+let usd = 0;
+
+fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&ids=plant-vs-undead-token')
+    .then((resp) => resp.json())
+    .then(function(data) {
+        console.log(data[0]);
+        $('#pvu-input').val(pvu);
+        $('#le-input').val(le);
+        $('#brl-input').val(data[0].current_price);
+        brl = data[0].current_price; 
+    });
+
+fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=plant-vs-undead-token')
+    .then((resp) => resp.json())
+    .then(function(data) {
+        $('#usd-input').val(data[0].current_price);
+        usd = data[0].current_price;
+    });
+
 (function() {
     let translations = {
         group1: "Grupo 1",
@@ -31,9 +53,8 @@ import moment from 'moment';
             let currentHour = moment(time, 'h:mm');
             let newHour = moment(time, 'h:mm').add(30, 'm');
 
-            console.log(currentHour);
-
-            if(currentHour.diff(now, 'h', true) > 0){
+            console.log(currentHour.diff(now, 'h', true));
+            if(currentHour.diff(now, 'h', true) > 0 && currentHour.diff(now, 'h', true) < 3){
                 next = ' - próximo';
             }
             
@@ -54,6 +75,31 @@ import moment from 'moment';
         setTimeGroup(value);
     });
 
+    $(document).on('change', '#pvu-input', function(){
+        console.log($(this).val() * brl);
+        $('#brl-input').val($(this).val() * brl);
+        $('#usd-input').val($(this).val() * usd);
+        $('#le-input').val($(this).val() * le);
+    });
+
+    $(document).on('change', '#usd-input', function(){
+        let rounded = ($(this).val() / usd).toFixed(2);
+        $('#pvu-input').val(rounded);
+        $('#brl-input').val((rounded * brl).toFixed(2));
+    });
+
+    $(document).on('change', '#brl-input', function(){
+        let rounded = ($(this).val() / brl).toFixed(2);
+        $('#pvu-input').val(rounded);
+        $('#usd-input').val((rounded * usd).toFixed(2));
+    });
+
+    $(document).on('change', '#le-input', function(){
+        let rounded = ($(this).val() / le).toFixed(2);
+        $('#pvu-input').val(rounded);
+        $('#usd-input').val(rounded * usd);
+        $('#brl-input').val(rounded * brl);
+    });
 
     chrome.storage.sync.get("group", ({ group }) =>{
         setTimeGroup(group || $('#group-plants').val());
